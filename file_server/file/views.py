bf9d1218def3286ec120262f60d339f68aa5d102
권주희 <akrso06197@khu.ac.kr>
2020-05-30 15:22:54 +0900

    add kafka producer code to api

--- file_server/file/views.py
+++ file_server/file/views.py
@@ -5,17 +5,22 @@ from django.http import HttpResponse
 import base64
 import json
 import os
+from kafka import KafkaProducer
+from kafka.errors import KafkaError
+
 class FileUploadView(APIView):
     parser_class = (FileUploadParser,)
     def post(self, request, *args, **kwargs):
         if request.method == 'POST':
             print("---- data in ----")
             befEncoding = request.POST['befEncoding']
-            print("DATA : ", befEncoding)
             userId = request.POST['userId']
             timeStamp = request.POST['timeStamp']
-            # bytesLike = bytes(befEncoding, encoding='utf8')
-            # print("BYTES : ", bytesLike)
+            encode = befEncoding.encode()
+            print("BYTE : ", encode)
+            bytesLike = bytes(befEncoding, encoding='utf8')
+            print("BYTES : ", bytesLike)
+
             # encoded_string = base64.b64encode(bytesLike)
             # print("Hey : ", encoded_string)
 
@@ -26,6 +31,21 @@ class FileUploadView(APIView):
 
             with open("media/"+userId+"/"+userId+"_"+timeStamp+".json", "w") as file:
                 json.dump(json_data,file,indent="\t")
+
+            producer = KafkaProducer(bootstrap_servers=['1.201.142.81:9092'])
+            future = producer.send('testTopic', befEncoding.encode())
+
+            try:
+                record_metadata = future.get(timeout=10)
+            except KafkaError:
+                # Decide what to do if produce request failed…
+                # log.exception()
+                pass
+
+            # Successful result returns assigned partition and offset
+            print(record_metadata.topic)
+            print(record_metadata.partition)
+            print(record_metadata.offset)
             print("---- process exit ----")
 
             return HttpResponse('save_success')
@@ -33,3 +53,5 @@ class FileUploadView(APIView):
           form = UploadFileForm()
         # return render(request, 'upload.html', {'form': form})
         return HttpResponse('/upload_failure')
+
+
