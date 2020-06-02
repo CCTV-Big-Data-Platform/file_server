d5cc507937f286f8088118d70fca7ec85838a48c
권주희 <akrso06197@khu.ac.kr>
2020-06-02 18:32:44 +0900

    update the user api

--- file_server/user/views.py
+++ file_server/user/views.py
@@ -3,7 +3,7 @@ from rest_framework.views import APIView
 from django.http import HttpResponse
 from django.db import models
 from .models import *
-from rest_framework.decorators import action
+from annoying.functions import get_object_or_None
 
 # Create your views here.
 class UserView(APIView):
@@ -11,8 +11,12 @@ class UserView(APIView):
         if request.method == 'POST':
             userId = request.POST['userId']
             userToken = request.POST['userToken']
-            user = User.objects.get(userId = userId)
-            if user:
+            print("ALL USER : ", User.objects.all())
+
+            user = get_object_or_None(User, userId=userId)
+            print("user : ", user)
+            if user != None:
+                print(user)
                 if userToken != '': #user에 토큰 입
                     user.userToken = userToken
                     user.save()
